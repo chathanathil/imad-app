55aee3f7524eae16caa7ada81104f27b066d16f1
chathanathil <36698410+chathanathil@users.noreply.github.com>
2018-03-06 16:34:16 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.get('/', function (req, res) {
       res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
      
  });
- app.get('/article-two',function(req,res){
-      res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+ app.get('/article-one',function(req,res){
+      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
  });
  
 app.get('/ui/style.css', function (req, res) {
