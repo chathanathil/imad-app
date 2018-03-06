cef28f649630e88eb695d9babd4bc55dc5cb2840
chathanathil <36698410+chathanathil@users.noreply.github.com>
2018-03-06 10:26:54 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
      
  });
  app.get('/article-two',function(req,res){
-     res.send('article two requested and serve here ');
+     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
  });
  
 app.get('/ui/style.css', function (req, res) {
